afba3470026f491d69646e8ae73a892f46005df5
ShaunR23 <shaunradford23@gmail.com>
2026-06-09 13:07:07 -0400

    adding gallery array with photos

---
+++ src/data/gallery.ts
@@ -0,0 +1,75 @@
+export const galleryItems = [
+
+    {
+        src: "/images/image0_1.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/image0.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/image1.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/image4.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/image5.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/image6.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/image7.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/image8.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/image9.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/image10.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/image11.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/image12.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/image13.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/image15.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/IMG_1184.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/IMG_1185.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/IMG_1186.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      },
+      {
+        src: "/images/IMG_1187.jpg",
+        alt: "Custom cake by Whisk Bake Repeat",
+      }
+]
